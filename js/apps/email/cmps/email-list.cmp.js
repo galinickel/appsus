aa84a2e7965aae842469bb4b2561e9465ec958eb
Almog <almoglemberg@gmail.com>
2021-02-24 17:12:29 +0200

    css error fixed

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -1,6 +1,9 @@
+import emailPreview from './email-preview.cmp.js'
+
 export default {
     template: `<div class="email-list">
         <h1> hi i'm email list</h1> 
-        </div>`
+        <emailPreview/>
+        </div>`,
+    components: emailPreview
 }
-/// this comp will be inside email-app
--- js/apps/email/pages/email-app.cmp.js
+++ js/apps/email/pages/email-app.cmp.js
@@ -1,17 +1,13 @@
 
 import emailCompose from '../cmps/email-compose.cmp.js'
-import emailList from '../cmps/email-list.cmp.js'
-import emailPreview from '../cmps/email-preview.cmp.js'
 
 export default {
     template: `<div class="app-page">
         <h1> hi i'm EMAIL APP </h1>
         <email-compose/>
-        <email-list/>
-        <email-preview/>
-        
+        <router-view></router-view>
         </div> `,
-    components: {emailCompose,
-    emailList,
-    emailPreview}
+    components: {
+        emailCompose
+    }
 }
