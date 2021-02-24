a91855a6ca771a3e582e76e246e44f79a0bf8d5b
Almog <almoglemberg@gmail.com>
2021-02-24 19:54:40 +0200

    what

--- js/site-services/util-service.js
+++ js/site-services/util-service.js
@@ -20,11 +20,3 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function makeId(length = 7) {
-    var text = '';
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
