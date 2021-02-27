f395d875aa8b8fddcbbd67673cc64b229d046d86
Gali Nickel <gali.nickel95@gmail.com>
2021-02-27 16:12:32 +0200

    css changes

--- js/apps/email/pages/email-details.cmp.js
+++ js/apps/email/pages/email-details.cmp.js
@@ -1,7 +1,7 @@
 import { emailService } from '../services/email-service.js';
 
-export default {
-    template: `<div v-if="email" class="email-details-container">
+export default { 
+    template: `<div v-if="email" class="email-details-container ">
         from: {{email.from}}
         <br>
         to: {{email.to}}
@@ -17,7 +17,6 @@ export default {
         return {
             email: null
         }
-
     },
     methods: {
         loadEmail() {
