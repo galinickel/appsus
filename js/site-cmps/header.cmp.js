fd8d84f4e1aa4f99d65af71422b9b0b0e2a3fb8c
Almog <almoglemberg@gmail.com>
2021-02-27 03:01:50 +0200

    css changes, more responsivity added

--- js/site-cmps/header.cmp.js
+++ js/site-cmps/header.cmp.js
@@ -2,10 +2,10 @@ export default {
     template: `
     <header>
     <div class="header-container flex"> 
-    <router-link :to="'/'"><img class="logo" src="imgs/logo_transparent.png"/></router-link>
+    <router-link :to="'/'"><img class="logo" src="imgs/rocking-horse.png"/></router-link>
     <ul class="header-navbar flex">
         <li><router-link :to="'/keep'">Keep</router-link></li>
-        <li><router-link :to="'/email/inbox'">Email</router-link></li>
+        <li><router-link :to="'/email/inbox'">Mail</router-link></li>
         <li><router-link :to="'/about'">About</router-link></li>
     </ul>
 </div>
--- js/site-pages/home-page.cmp.js
+++ js/site-pages/home-page.cmp.js
@@ -2,10 +2,14 @@ export default {
     template: `<div class="app-page home-page flex">
         <h1 class="main-title">No need to swing between apps.</h1>
     <div class="home-container flex">
-    <h1 class="app-title">Keep</h1>
+    <h1 class="app-title" @click="routeTo('keep')">Keep</h1>
             <img src="imgs/rocking-horse.svg" alt="" class="horse">
-            <h1 class="app-title">Mail</h1>
-
+            <h1 class="app-title" @click="routeTo('email/inbox')">Mail</h1>
             </div>
-        </div>`
+        </div>`,
+    methods: {
+        routeTo(app) {
+            this.$router.push(`/${app}`)
+        }
+    }
 }
