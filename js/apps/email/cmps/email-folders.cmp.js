5041c10bdbdc10b1b9840183a4bf4da172c41d2f
Almog <almoglemberg@gmail.com>
2021-02-27 03:14:37 +0200

    keep notes more responsivity added

--- js/apps/email/cmps/email-folders.cmp.js
+++ js/apps/email/cmps/email-folders.cmp.js
@@ -8,8 +8,14 @@ export default {
             </div>
             <div class="folder" :class="{'active-folder': activeFolder === 'sent'}" @click="setActiveFolder('sent')"><i class="far fa-paper-plane"></i> Sent</div>
             <div class="folder" :class="{'active-folder': activeFolder === 'all'}" @click="setActiveFolder('all')">All</div>
-            <!-- <li>Drafts</li> -->
-            <!-- <li>Trash</li> -->
+
+            <!-- TODO: MORE FOLDERS- DRAFTS, TRASH, IMPORTANT -->
+
+            <!-- <div class="folder" :class="{'active-folder': activeFolder === 'drafts'}" @click="setActiveFolder('drafts')>Drafts</div> -->
+            
+            <!-- <div class="folder" :class="{'active-folder': activeFolder === 'important'}" @click="setActiveFolder('important')>Important</div> -->
+
+            <!-- <div class="folder" :class="{'active-folder': activeFolder === 'trash'}" @click="setActiveFolder('trash')>Trash</div> -->
 </section>`,
     data() {
         return {
