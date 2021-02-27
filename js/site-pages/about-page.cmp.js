a1804835b5f2493d04c3e8424af4b92bd43e069d
Gali Nickel <gali.nickel95@gmail.com>
2021-02-27 02:54:26 +0200

    about page

--- js/site-pages/about-page.cmp.js
+++ js/site-pages/about-page.cmp.js
@@ -1,5 +1,21 @@
 export default {
     template: `<div class="app-page">
-        <h1>ABOUT-PAGE</h1>
+        <div class="about-container main-layout flex">
+            <h1 class="">What is APPSUS?</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum officiis modi excepturi facilis dolore quos nostrum corporis perferendis aperiam et provident vitae non suscipit nihil, nemo autluptatibus obcaecati.</p>
+        <div class="team-container">
+            <h1 class="">Our Team</h1>
+            <div class="team-info-container flex">
+                <div class="team-member flex">
+                    <img src="https://p7.hiclipart.com/preview/722/101/213/computer-icons-user-profile-circle-abstract.jpg" >
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolorem accusantium expedita quasi veritatis eum, odio ratione libero totam deserunt vero sint assumenda, enim tempora.</p>
+                </div>
+                <div class="team-member flex">
+                    <img src="https://p7.hiclipart.com/preview/722/101/213/computer-icons-user-profile-circle-abstract.jpg" >
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem dolorem accusantium expedita quasi veritatis eum, odio ratione libero totam deserunt vero sint assumenda, enim tempora.</p>
+                </div>
+            </div>
+        </div>
+        </div>
         </div>`
 }
